596cfc3b473cac43b664030fa26a215bee9ff52e
Emelda Umahi <memelxxii@gmail.com>
2025-09-08 11:52:32 +0100

    new changes

--- client/components/Filter.tsx
+++ client/components/Filter.tsx
@@ -16,7 +16,7 @@ export default function Filter(){
         <div className="flex items-center justify-end gap-2 text-sm text-gray-500 my-6">
             <span>Sort by:</span>
             <select className="ring-1 ring-gray-200 shadow-md p-1 rounded-sm"
-            onChange={() => handleFilter(e.target.value)} name="sort" id="sort">
+            onChange={(e) => handleFilter(e.target.value)} name="sort" id="sort">
                 <option value="newest">Newest</option>
                 <option value="oldest">Oldest</option>
                 <option value="asc">Price: Low to High</option>
