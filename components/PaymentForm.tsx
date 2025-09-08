efefb82add6edad771b2817c4b5002537825200d
Emelda Umahi <memelxxii@gmail.com>
2025-09-08 11:25:19 +0100

    Add admin Next.js app

--- components/PaymentForm.tsx
+++
@@ -1,5 +0,0 @@
-export default function PaymentForm(){
-    return(
-        <div></div>
-    )
-}
--- components/ShippingForm.tsx
+++
@@ -1,5 +0,0 @@
-export default function ShippingForm(){
-    return(
-        <div></div>
-    )
-}
